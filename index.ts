fde36c326f24769194c7de677ebb13fd77803d4e
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-06 23:03:46 -0800

    :sparkles: Add basic package

--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
-export const show = () => {
-  return "PK";
+export const hide = (element: HTMLElement, ms = 300) => {
+  if (!element) return;
+  const currentTransition = element.style.transition;
+  element.style.transition = `opacity ${ms}ms`;
+  requestAnimationFrame(() => {
+    element.style.opacity = "0";
+    setTimeout(() => {
+      element.style.display = "none";
+      element.setAttribute("hidden", "hidden");
+      element.style.transition = currentTransition;
+    }, ms);
+  });
+}
+
+export const show = (element: HTMLElement, ms = 300) => {
+  if (!element) return;
+  if (element.hasAttribute("hidden")) return;
+  element.style.opacity = "0";
+  element.style.display = "";
+  element.removeAttribute("hidden");
+  const currentTransition = element.style.transition;
+  element.style.transition = `opacity ${ms}ms`;
+  requestAnimationFrame(() => {
+    element.style.opacity = "1";
+    setTimeout(() => {
+      element.style.transition = currentTransition;
+    }, ms);
+  });
 }
